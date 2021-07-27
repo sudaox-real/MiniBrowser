0e8410da78cff540abf9a3e2e6c80e2c13f345e9
Sudaox <75401250+sudaox-real@users.noreply.github.com>
2021-07-27 13:10:49 +0100

    Add design elements

--- MiniBrowser/Form1.Designer.cs
+++ MiniBrowser/Form1.Designer.cs
@@ -29,20 +29,55 @@ namespace MiniBrowser
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.go = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(704, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // go
+            // 
+            this.go.Location = new System.Drawing.Point(723, 2);
+            this.go.Name = "go";
+            this.go.Size = new System.Drawing.Size(75, 23);
+            this.go.TabIndex = 1;
+            this.go.Text = "Go";
+            this.go.UseVisualStyleBackColor = true;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(12, 30);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(776, 408);
+            this.webBrowser1.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.go);
+            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "MiniBrowser";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button go;
+        private System.Windows.Forms.WebBrowser webBrowser1;
     }
 }
 
